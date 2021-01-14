c2712e79d190d34c7a65477573558ba8da175065
Alex Khmelev <sashaaka4774@gmail.com>
2021-01-14 14:45:12 +0300

    Initialize strybook
    Storybook & small change

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
+export interface ExampleComponentProps {
   text: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
+export const ExampleComponent = ({ text }: ExampleComponentProps) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
