9afb27ede0702819984cd65e5f03fe4154167b92
Aleksandr Khmelev <sashaaka4774@gmail.com>
2023-07-13 19:45:26 +0300

    Move old files to common

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { Header, HeaderProps } from '../Components/common/Header';
 
-import { Header, HeaderProps } from './Header';
 
 export default {
     title: 'Example/Header',
--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { Page, PageProps } from '../Components/common/Page';
+import HeaderStories from './Header.stories';
 
-import { Page, PageProps } from './Page';
-import * as HeaderStories from './Header.stories';
 
 export default {
     title: 'Example/Page',
